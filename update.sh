6521ebf46454e58308cc4b373776dd34d71ba270
hengist <2325135260@qq.com>
2022-06-28 17:39:32 +0800

    .

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
-cp ~/.zshrc ./zshrc
-cp -r ~/.doom.d ./doom.d
+cp ~/.doom.d/config.el  	doom.d/config.el
+cp ~/.doom.d/init.el     	doom.d/init.el  
+cp ~/.doom.d/packages.el  	doom.d/packages.el
+cp ~/.zshrc 			zshrc
