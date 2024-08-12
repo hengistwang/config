903b14d966f4e3de31f5ff1dedcff5010e81fb1a
hengist <hengistwang666@gmail.com>
2024-08-12 21:01:04 +0800

    fuck

--- doom/init.el
+++ doom/init.el
@@ -37,7 +37,7 @@
        ;;hydra
        indent-guides
        ligatures
-       minimap
+       ;;minimap
        modeline
        nav-flash
        ;;neotree
@@ -90,7 +90,7 @@
        ;;(debugger +lsp)
        ;;direnv
        ;;docker
-       editorconfig
+       ;;editorconfig
        ;;ein
        ;;(eval +overlay)
        ;;gist
@@ -135,7 +135,7 @@
        ;;fsharp
        ;;fstar
        ;;gdscript
-       ;;(go +lsp +tree-sitter)
+       (go +lsp +tree-sitter)
        ;;(graphql +lsp)
        (haskell +lsp +tree-sitter)
        ;;hy
